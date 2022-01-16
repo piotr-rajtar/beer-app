370b5ff0793b3effe1c13ece92ea1d1530191241
Piotr Rajtar <piotrek.rajtar@gmail.com>
2022-01-16 12:28:28 +0100

    Add loader component

---
+++ src/components/Loader.vue
@@ -0,0 +1,28 @@
+<template><div :class="style.spinner" /></template>
+
+<script lang="ts">
+import { Options, Vue } from 'vue-class-component';
+
+@Options({})
+export default class Loader extends Vue {}
+</script>
+
+<style scoped lang="scss" module="style">
+.spinner {
+  width: 50px;
+  height: 50px;
+  border-radius: 50%;
+  background: radial-gradient(farthest-side, #ffa516 94%, #0000) top/8px 8px
+      no-repeat,
+    conic-gradient(#0000 30%, #ffa516);
+  mask: radial-gradient(farthest-side, #0000 calc(100% - 8px), #000 0);
+  -webkit-mask: radial-gradient(farthest-side, #0000 calc(100% - 8px), #000 0);
+  animation: s3 1s infinite linear;
+}
+
+@keyframes s3 {
+  100% {
+    transform: rotate(1turn);
+  }
+}
+</style>
