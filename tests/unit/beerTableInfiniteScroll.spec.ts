fe482dc6751ef21b4799ea313db984da1fe1cc68
Piotr Rajtar <piotrek.rajtar@gmail.com>
2022-08-28 16:09:01 +0200

    Add rest test cases for infiniteScroll

--- tests/unit/beerTableInfiniteScroll.spec.ts
+++ tests/unit/beerTableInfiniteScroll.spec.ts
@@ -7,6 +7,8 @@ import storeCreator from '@/store/index';
 describe('BeerTableInfiniteScroll.vue', () => {
   let testStore: Store<State>;
   let wrapper: VueWrapper<InstanceType<typeof BeerTableInfiniteScroll>>;
+  const addEventListener: jest.SpyInstance = jest.spyOn(window, 'addEventListener');
+  const removeEventListener = jest.spyOn(window, 'removeEventListener');
 
   beforeEach(() => {
     testStore = storeCreator();
@@ -14,7 +16,7 @@ describe('BeerTableInfiniteScroll.vue', () => {
       attachTo: document.body,
       computed: {
         numberOfInitialFetchNeeded() {
-          return 2;
+          return 1;
         },
       },
       global: {
@@ -26,6 +28,20 @@ describe('BeerTableInfiniteScroll.vue', () => {
     });
   });
 
+  it('adds "scroll" event listener when component mounted', () => {
+    expect(addEventListener).toHaveBeenCalled();
+  });
+  it('removes "scroll" event listener when component unmounted', () => {
+    wrapper.unmount();
+    expect(removeEventListener).toHaveBeenCalled();
+  });
+  it('removes "scroll" event listener when all data fetched', () => {
+    const areAllDataFetchedWatcher = wrapper.vm.$options.watch?.areAllDataFetched as (
+      areAllDataFetched: boolean
+    ) => void;
+    areAllDataFetchedWatcher.call(wrapper.vm, true);
+    expect(removeEventListener).toHaveBeenCalled();
+  });
   it('emits "loadMore" event when component mounted', () => {
     expect(wrapper.emitted().hasOwnProperty('loadMore')).toBe(true);
   });
@@ -36,14 +52,4 @@ describe('BeerTableInfiniteScroll.vue', () => {
     const expectedNumberOfEmits: number = wrapper.vm.numberOfInitialFetchNeeded * 2;
     expect(wrapper.emitted().loadMore.length).toBe(expectedNumberOfEmits);
   });
-  it('removes "scroll" event listener when all data fetched', () => {
-    const removeEventListener = jest.spyOn(window, 'removeEventListener');
-    const areAllDataFetchedWatcher = wrapper.vm.$options.watch?.areAllDataFetched as (
-      areAllDataFetched: boolean
-    ) => void;
-    areAllDataFetchedWatcher.call(wrapper.vm, true);
-    expect(removeEventListener).toHaveBeenCalled();
-  });
-  //it('removes "scroll" event listener when component unmounted', () => {}),
-  //it('add "scroll" event listener when component mounted', () => {}),
 });
